5bda88c3027fdbdf3d9075cd5e2c24bc8f5dfc6a
forgoty <forgoty13@gmail.com>
2025-09-14 17:01:35 +0200

    Change config

--- config.h
+++ config.h
@@ -1,14 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",    "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	{"",	"sb-forecast",	3600,	14},
 	{"",	"sb-disk",	3600,	15},
-	{"",	"sb-cpubars",	10,	19},
+	{"",	"sb-cpubars",	5,	19},
 	{"",	"sb-cpu",	10,	16},
 	{"",	"sb-memory",	10,	17},
 	{"",	"sb-nettraf",	1,	18},
 	{"",	"sb-volume",	0,	10},
+	{"",	"sb-brightness",10,	2},
 	{"",	"sb-battery",	5,	3},
 	{"",	"sb-clock",	60,	1},
 	{"",	"sb-internet",	5,	4},
